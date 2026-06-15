e90a39ba917ebbd552e04b1beb6e6b37f2fc2471
Bhavya-Dhanawani <bhavyadhanwani1234@gmail.com>
2026-06-15 21:21:17 +0530

    refreshtoken changed

--- server/src/shared/constants/cookies.constants.js
+++ server/src/shared/constants/cookies.constants.js
@@ -1,7 +1,9 @@
+import env from "../config/env.config.js";
+
 export const refreshConstants = {
     httpOnly: true,
-    secure: process.env.NODE_ENV === "production",
-    sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+    secure: env.NODE_ENV === "production",
+    sameSite: env.NODE_ENV === "production" ? "none" : "lax",
     maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
-    path: "/api/auth"
+    path: "/api"
 }
